957249cc957e716d118ed1f504aea6de092fecab
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2020-02-23 17:44:19 +0700

    base

--- js/blog.js
+++ js/blog.js
@@ -1,3 +1,4 @@
 $(".BlogSearch input.gsc-input").attr("placeholder", "Type here..");
 addClass(".home-link", "btn btn-danger");
 addClass(".BlogSearch input.gsc-search-button", "btn btn-danger");
+$(".BlogSearch input.gsc-search-button").replaceWith("<button class='gsc-search-button btn btn-danger' title='search' type='submit'><i class='fa fa-search' aria-hidden='true'></i></button>");
