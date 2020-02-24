d68b0dfd3a4ea3fd4fb2c093594b85e9e9ed6b64
Nauval Muhammad Firdaus <nauvalmfirdaus@gmail.com>
2020-02-24 17:36:57 +0700

    base

--- js/blog.js
+++ js/blog.js
@@ -2,3 +2,7 @@ $(".BlogSearch input.gsc-input").attr("placeholder", "Type here..");
 addClass(".home-link", "btn btn-danger");
 addClass(".BlogSearch input.gsc-search-button", "btn btn-danger");
 $(".BlogSearch input.gsc-search-button").replaceWith("<button class='gsc-search-button btn btn-danger' title='search' type='submit'><i class='fa fa-search' aria-hidden='true'></i></button>");
+$(".blog-pager-older-link").html("Next");
+$(".blog-pager-newer-link").html("Prev");
+addClass(".blog-pager-older-link", "btn btn-danger");
+addClass(".blog-pager-newer-link", "btn btn-danger");
